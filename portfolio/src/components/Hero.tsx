97e0b7b31f23b7668c2c74fa75b88e8d405c7a37
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2026-07-13 13:20:14 +0100

    change title text size

--- portfolio/src/components/Hero.tsx
+++ portfolio/src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
     >
       <div className="absolute bottom-10 left-1/2 w-full max-w-8xl -translate-x-1/2 px-6 text-center xs:bottom-6">
         <h3 className="flex flex-col items-center gap-4 text-lg lg:text-4xl text-white sm:flex-row sm:justify-center ">
-          <span className="bg-black/10 ">
+          <span className="bg-black/20 ">
             Producer and mixer based in Brighton, UK
           </span>
           <span className="flex gap-4 text-2xl">
